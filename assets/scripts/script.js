aed3b3c2b21cc744b45cacd430485741412555b9
gderuwez <gderuwez@gmail.com>
2018-06-12 13:10:25 +0200

    service worker reg fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/One_page_sosEnfant/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('/One_page_sosEnfant-Guillaume/service-worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
